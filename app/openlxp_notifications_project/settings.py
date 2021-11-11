adccb10c4a96616814e5fa190c30acf597acce08
KarenAJ <kjijo@deloitte.com>
2021-11-11 14:41:54 -0500

    Added edits to correct settings.py

--- app/openlxp_notifications_project/settings.py
+++ app/openlxp_notifications_project/settings.py
@@ -22,19 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY_VAL')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 mimetypes.add_type("text/css", ".css", True)
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'openlxp_notifications_project.wsgi.application'
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -121,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -135,7 +131,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -144,7 +139,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/openlxp_notifications/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 LOG_PATH = os.environ.get('LOG_PATH')
 LOGGING = {
@@ -183,4 +177,3 @@ LOGGING = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-
