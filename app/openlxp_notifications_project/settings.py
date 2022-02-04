5e6268100426324ae6a8303d941d98c0c4328830
KJIJO <kjijo@deloitte.com>
2022-02-04 12:14:16 -0500

    Made changes to settings for testing

--- app/openlxp_notifications_project/settings.py
+++ app/openlxp_notifications_project/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY_VAL')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 mimetypes.add_type("text/css", ".css", True)
 
